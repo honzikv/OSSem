3732bba33893120d5b797a7b711526daa94a153e
nvk <vaclavhonzik98@gmail.com>
2021-11-08 21:15:27 +0100

    logging

--- src/kernel/Utils/Semaphore.cpp
+++ src/kernel/Utils/Semaphore.cpp
@@ -8,12 +8,10 @@ void Semaphore::Acquire() {
 		condition_variable.wait(lock);
 	}
 	count -= 1;
-	LogDebug("Semaphore acquired");
 }
 
 void Semaphore::Release() {
 	std::scoped_lock lock(mutex);
-	LogDebug("Releasing lock");
 	count += 1;
 	condition_variable.notify_one();
 }
--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -8,6 +8,7 @@
 #include "IO/ConsoleIn.h"
 #include "IO/ConsoleOut.h"
 #include "Process/ProcessManager.h"
+#include "Utils/Config.h"
 
 
 void Initialize_Kernel() {
@@ -59,7 +60,8 @@ void __stdcall Bootstrap_Loader(kiv_hal::TRegisters& context) {
 	shell_regs.rax.h = static_cast<decltype(shell_regs.rax.h)>(kiv_os::NOS_Service_Major::Process);
 	shell_regs.rax.l = static_cast<decltype(shell_regs.rax.l)>(kiv_os::NOS_Process::Clone);
 	shell_regs.rcx.l = static_cast<decltype(shell_regs.rcx.l)>(kiv_os::NClone::Create_Process);
-	shell_regs.rdx.r = reinterpret_cast<decltype(shell_regs.rdx.r)>(shell_command); // rdx je pretypovany pointer na jmeno souboru
+	shell_regs.rdx.r = reinterpret_cast<decltype(shell_regs.rdx.r)>(shell_command);
+	// rdx je pretypovany pointer na jmeno souboru
 	shell_regs.rdi.r = reinterpret_cast<decltype(shell_regs.rdi.r)>(shell_args); // rdi je pointer na argumenty
 	shell_regs.rbx.e = std_in_handle << 16 | std_out_handle; // rbx obsahuje stdin a stdout
 	Sys_Call(shell_regs);
@@ -73,7 +75,13 @@ void __stdcall Bootstrap_Loader(kiv_hal::TRegisters& context) {
 	init_regs.rdx.r = reinterpret_cast<decltype(init_regs.rdx.r)>(&shell_pid); // pointer na shell_pid
 	init_regs.rcx.e = 1; // pouze jeden prvek
 	Sys_Call(init_regs);
-	LogDebug("Init is Gone ");
+
+	// TODO remove
+#if IS_DEBUG
+	LogDebug("Init process has been killed. Kernel would die here if it wasn't in debug");
+	while (true) { }
+#endif
+
 	Shutdown_Kernel();
 }
 
