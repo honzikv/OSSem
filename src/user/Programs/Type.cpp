581590f4ab2bb97f0d9bec7d030f177a6c6cd5ae
Schwobik <michal.schwob@seznam.cz>
2021-12-02 10:06:41 +0100

    nefunkci type

--- src/user/Programs/Type.cpp
+++ src/user/Programs/Type.cpp
@@ -10,7 +10,7 @@ size_t __stdcall type(const kiv_hal::TRegisters& regs) {
 	size_t read = 0;
 	size_t position_in_file = 0;
 
-	constexpr size_t read_buffer_size = 256;
+	constexpr size_t read_buffer_size = 1024;
 	std::vector<char> read_buffer(read_buffer_size);
 
 	kiv_os::THandle handler_in;
@@ -25,27 +25,45 @@ size_t __stdcall type(const kiv_hal::TRegisters& regs) {
 		return 1;
 	}
 
-	const auto result = kiv_os_rtl::Open_File(handler_in, args, kiv_os::NOpen_File::fmOpen_Always, kiv_os::NFile_Attributes::System_File);
+	kiv_os::NFile_Attributes file_attributes;
+	kiv_os_rtl::Get_File_Attributes(args, file_attributes);
+	if (file_attributes == kiv_os::NFile_Attributes::Directory) {
+		// je to dir -> nebudu vypisovat
+		const std::string message("Access denied.\n");
 
-	if (handler_in == static_cast<kiv_os::THandle>(-1)) {
-		constexpr auto exit_code = static_cast<uint16_t>(kiv_os::NOS_Error::File_Not_Found);
-
-		kiv_os_rtl::Exit(exit_code);
-		return 0;
+		size_t written;
+		kiv_os_rtl::Write_File(std_out, message.data(), message.size(), written);
+		kiv_os_rtl::Exit(static_cast<uint16_t>(kiv_os::NOS_Error::Invalid_Argument));
+		return 1;
 	}
 
-	//// Nastavi pozici v souboru na zacatek
-	//kiv_os_rtl::Seek(handler_in, kiv_os::NFile_Seek::Set_Position, kiv_os::NFile_Seek::Beginning, position_in_file);
+	if (!kiv_os_rtl::Open_File(handler_in, args, kiv_os::NOpen_File::fmOpen_Always, kiv_os::NFile_Attributes::System_File)) {
+		// soubor se neotevrel
+		kiv_os_rtl::Exit(static_cast<uint16_t>(kiv_os::NOS_Error::File_Not_Found));
+		return 1;
+	}
 
-	kiv_os_rtl::Read_File(handler_in, read_buffer.data(), read_buffer_size, read);
+    // Nastavi pozici v souboru na zacatek
+    kiv_os_rtl::Seek(handler_in, kiv_os::NFile_Seek::Set_Position, kiv_os::NFile_Seek::Beginning, position_in_file);
+
 	while (read) {
-		output.append(read_buffer.data(), read);
-		
-		position_in_file += read;
 		// nastavi pozici v souboru za konec prectene casti
-		//kiv_os_rtl::Seek(handler_in, kiv_os::NFile_Seek::Set_Position, kiv_os::NFile_Seek::Beginning, position_in_file);
-		kiv_os_rtl::Read_File(handler_in, read_buffer.data(), read_buffer_size, read);
-	} 
+		kiv_os_rtl::Seek(handler_in, kiv_os::NFile_Seek::Set_Position, kiv_os::NFile_Seek::Beginning, position_in_file);
+		if (!kiv_os_rtl::Read_File(handler_in, read_buffer.data(), read_buffer_size, read)) {
+			// je to dir -> nebudu vypisovat
+			const std::string message("IO Error.\n");
+
+			size_t written;
+			kiv_os_rtl::Write_File(std_out, message.data(), message.size(), written);
+			kiv_os_rtl::Exit(static_cast<uint16_t>(read));
+			return 1;
+		}
+
+		output.append(read_buffer.data(), read);
+
+		position_in_file += read;
+	}
+	output.append("\n");
 
 	size_t written;
 	kiv_os_rtl::Write_File(std_out, output.data(), output.size(), written);
