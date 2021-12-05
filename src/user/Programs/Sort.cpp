9a826a20c195d11a191d461ecfc21c94c2c3711e
Schwobik <michal.schwob@seznam.cz>
2021-12-05 23:10:30 +0100

    Oprava Sortu

--- src/user/Programs/Sort.cpp
+++ src/user/Programs/Sort.cpp
@@ -9,39 +9,40 @@ size_t __stdcall sort(const kiv_hal::TRegisters &regs) {
 	size_t written;
 	std::string output;
 
-	size_t actual_position = 0;
 	kiv_os::THandle handler_in = std_in;
 	bool is_file = false;
 	if (!args.empty()) {
-		kiv_os_rtl::Open_File(handler_in, args, kiv_os::NOpen_File::fmOpen_Always, kiv_os::NFile_Attributes::System_File);
+		size_t actual_position = 0;
+		if (!kiv_os_rtl::Open_File(handler_in, args, kiv_os::NOpen_File::fmOpen_Always, kiv_os::NFile_Attributes::System_File)) {
+			auto err_message = std::string("Can not open file ");
+			err_message.append(args);
+			err_message.append(".\n");
+			kiv_os_rtl::Write_File(std_out, err_message.data(), err_message.size(), written);
 
-		if (handler_in == static_cast<kiv_os::THandle>(-1)){
 			kiv_os_rtl::Exit(static_cast<uint16_t>(kiv_os::NOS_Error::File_Not_Found));
-			return 0;
+			return 1;
 		}
 
+		
 		is_file = true;
 		kiv_os_rtl::Seek(handler_in, kiv_os::NFile_Seek::Set_Position, kiv_os::NFile_Seek::Beginning, actual_position);
 	}
 
 	constexpr int buffer_size = 256;
-	size_t read;
+	size_t read = 1;
 	std::vector<char> read_buffer(buffer_size);
 	std::string input_data;
 
-	kiv_os_rtl::Read_File(handler_in, read_buffer.data(), buffer_size, read);
 	while (read) {
-		//pokud je prvni znak konec souboru nebo ukonceni vstupu (CTRL+Z)
-		if (read_buffer[0] == static_cast<char>(kiv_hal::NControl_Codes::SUB)) {
+		if (!kiv_os_rtl::Read_File(handler_in, read_buffer.data(), buffer_size, read)) {
+			// uz tam nejspis nic neni
+			break;
+		}
+		//pokud je posledni precteny znak konec souboru nebo ukonceni vstupu (CTRL+Z)
+		if (read_buffer[read - 1] == static_cast<char>(kiv_hal::NControl_Codes::SUB)) {
 			break;
 		}
 		input_data.append(read_buffer.data(), 0, read);
-
-		if (is_file) {
-			actual_position += read;
-			kiv_os_rtl::Seek(handler_in, kiv_os::NFile_Seek::Set_Position, kiv_os::NFile_Seek::Beginning, actual_position);
-		}
-		kiv_os_rtl::Read_File(handler_in, read_buffer.data(), buffer_size, read);
 	}
 
 	if (is_file) {
@@ -64,9 +65,9 @@ size_t __stdcall sort(const kiv_hal::TRegisters &regs) {
 
 	for (const auto& it : lines) {
 		output.append(it);
+		output.append("\n");
 	}
 	
-	output.append("\n");
 	kiv_os_rtl::Write_File(std_out, output.data(), output.size(), written);
 	
 	kiv_os_rtl::Exit(static_cast<uint16_t>(kiv_os::NOS_Error::Success));
